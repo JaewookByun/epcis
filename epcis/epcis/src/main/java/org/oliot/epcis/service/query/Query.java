ca60ea0b64043d1a2bf5c065fd30b0b87ef58489
JaewookByun <bjw0829@gmail.com>
2014-09-01 15:18:39 +0900

    Query Service: getQueryNames, getStandardVersion, getVendorVersion

--- epcis/epcis/src/main/java/org/oliot/epcis/service/query/Query.java
+++ epcis/epcis/src/main/java/org/oliot/epcis/service/query/Query.java
@@ -1,64 +1,164 @@
+/**
+ * Copyright (C) 2006, 2005 EPCglobal Inc., All Rights Reserved.
+ * EPCglobal Inc., its members, officers, directors, employees, or agents shall
+ * not be liable for any injury, loss, damages, financial or otherwise,
+ *	arising from, related to, or caused by the use of this document. 
+ * The use of said document shall constitute your express consent 
+ * to the foregoing exculpation.
+ */
+
 package org.oliot.epcis.service.query;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.oliot.model.epcis.QueryParams;
 import org.oliot.model.epcis.QueryResults;
 import org.oliot.model.epcis.SubscriptionControls;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/query")
 public class Query implements CoreQueryService {
 
+	/**
+	 * Registers a subscriber for a previously defined query having the
+	 * specified name. The params argument provides the values to be used for
+	 * any named parameters defined by the query. The dest parameter specifies a
+	 * destination where results from the query are to be delivered, via the
+	 * Query Callback Interface. The dest parameter is a URI that both
+	 * identifies a specific binding of the Query Callback Interface to use and
+	 * specifies addressing information. The controls parameter controls how the
+	 * subscription is to be processed; in particular, it specifies the
+	 * conditions under which the query is to be invoked (e.g., specifying a
+	 * periodic schedule). The subscriptionID is an arbitrary string that is
+	 * copied into every response delivered to the specified destination, and
+	 * otherwise not interpreted by the EPCIS service. The client may use the
+	 * subscriptionID to identify from which subscription a given result was
+	 * generated, especially when several subscriptions are made to the same
+	 * destination. The dest argument MAY be null or empty, in which case
+	 * results are delivered to a pre-arranged destination based on the
+	 * authenticated identity of the caller. If the EPCIS implementation does
+	 * not have a destination pre-arranged for the caller, or does not permit
+	 * this usage, it SHALL raise an InvalidURIException.
+	 */
 	@Override
 	public void subscribe(String queryName, QueryParams params, URI dest,
 			SubscriptionControls controls, String subscriptionID) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	/**
+	 * Removes a previously registered subscription having the specified
+	 * subscriptionID.
+	 */
 	@Override
 	public void unsubscribe(String subscriptionID) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
-	public QueryResults poll(String queryName, QueryParams params) {
-		
-		return null;
-	}
-
-	@Override
-	public List<String> getQueryNames() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+	/**
+	 * Returns a list of all subscriptionIDs currently subscribed to the
+	 * specified named query.
+	 */
 	@Override
 	public List<String> getSubscriptionIDs(String queryName) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	
+	
+	
+	
+	
+	/**
+	 * Invokes a previously defined query having the specified name, returning
+	 * the results. The params argument provides the values to be used for any
+	 * named parameters defined by the query.
+	 */
 	@Override
-	@RequestMapping(value = "/StandardVersion", method=RequestMethod.GET)
+	public QueryResults poll(String queryName, QueryParams params) {
+		params.getParam();
+		return null;
+	}
+
+	/**
+	 * [REST Version of getQueryNames]
+	 * Returns a list of all query names available for use with the subscribe
+	 * and poll methods. This includes all pre- defined queries provided by the
+	 * implementation, including those specified in Section 8.2.7.
+	 * @return JSONArray of query names ( String )
+	 */
+	@RequestMapping(value = "/QueryNames", method = RequestMethod.GET)
+	@ResponseBody
+	public String getQueryNamesREST() {
+		JSONArray jsonArray = new JSONArray();
+		List<String> queryNames = getQueryNames();
+		for(int i = 0 ; i < queryNames.size() ; i++ )
+		{
+			jsonArray.put(queryNames.get(i));
+		}
+		return jsonArray.toString(1);
+	}
+
+	/**
+	 * Returns a list of all query names available for use with the subscribe
+	 * and poll methods. This includes all pre- defined queries provided by the
+	 * implementation, including those specified in Section 8.2.7.
+	 * 
+	 * 
+	 */
+	@Override
+	public List<String> getQueryNames() {
+		List<String> queryNames = new ArrayList<String>();
+		queryNames.add("SimpleEventQuery");
+		return queryNames;
+	}
+
+	/**
+	 * Returns a string that identifies what version of the specification this
+	 * implementation complies with. The possible values for this string are
+	 * defined by GS1. An implementation SHALL return a string corresponding to
+	 * a version of this specification to which the implementation fully
+	 * complies, and SHOULD return the string corresponding to the latest
+	 * version to which it complies. To indicate compliance with this Version
+	 * 1.1 of the EPCIS specification, the implementation SHALL return the
+	 * string 1.1.
+	 */
+	@Override
+	@RequestMapping(value = "/StandardVersion", method = RequestMethod.GET)
+	@ResponseBody
 	public String getStandardVersion() {
 		return "1.1";
 	}
 
+	/**
+	 * Returns a string that identifies what vendor extensions this
+	 * implementation provides. The possible values of this string and their
+	 * meanings are vendor-defined, except that the empty string SHALL indicate
+	 * that the implementation implements only standard functionality with no
+	 * vendor extensions. When an implementation chooses to return a non-empty
+	 * string, the value returned SHALL be a URI where the vendor is the owning
+	 * authority. For example, this may be an HTTP URL whose authority portion
+	 * is a domain name owned by the vendor, a URN having a URN namespace
+	 * identifier issued to the vendor by IANA, an OID URN whose initial path is
+	 * a Private Enterprise Number assigned to the vendor, etc.
+	 */
 	@Override
-	@RequestMapping(value = "/VendorVersion", method=RequestMethod.GET)
+	@RequestMapping(value = "/VendorVersion", method = RequestMethod.GET)
+	@ResponseBody
 	public String getVendorVersion() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	
-	
-	
 }
