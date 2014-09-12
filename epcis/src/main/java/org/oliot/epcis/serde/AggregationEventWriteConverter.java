9b80bdb7a3cbd1c2c51527c14f0b3029528d247d
JaewookByun <bjw0829@gmail.com>
2014-09-12 10:53:00 +0900

    Comment Re-written

--- epcis/src/main/java/org/oliot/epcis/serde/AggregationEventWriteConverter.java
+++ epcis/src/main/java/org/oliot/epcis/serde/AggregationEventWriteConverter.java
@@ -36,13 +36,22 @@ import org.w3c.dom.NamedNodeMap;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
+
 /**
  * Copyright (C) 2014 KAIST RESL
  *
- * This file is part of Oliot (oliot.org).
+ * This project is part of Oliot (oliot.org), pursuing the implementation of
+ * Electronic Product Code Information Service(EPCIS) v1.1 specification in
+ * EPCglobal.
+ * [http://www.gs1.org/gsmp/kc/epcglobal/epcis/epcis_1_1-standard-20140520.pdf]
+ * 
  *
- * @author Jack Jaewook Byun, Ph.D student Korea Advanced Institute of Science
- *         and Technology Real-time Embedded System Laboratory(RESL)
+ * @author Jack Jaewook Byun, Ph.D student
+ * 
+ *         Korea Advanced Institute of Science and Technology (KAIST)
+ * 
+ *         Real-time Embedded System Laboratory(RESL)
+ * 
  *         bjw0829@kaist.ac.kr
  */
 @Component
--- epcis/src/main/java/org/oliot/epcis/service/query/QueryTest.java
+++ epcis/src/main/java/org/oliot/epcis/service/query/QueryTest.java
@@ -17,7 +17,23 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 
-
+/**
+ * Copyright (C) 2014 KAIST RESL
+ *
+ * This project is part of Oliot (oliot.org), pursuing the implementation of
+ * Electronic Product Code Information Service(EPCIS) v1.1 specification in
+ * EPCglobal.
+ * [http://www.gs1.org/gsmp/kc/epcglobal/epcis/epcis_1_1-standard-20140520.pdf]
+ * 
+ *
+ * @author Jack Jaewook Byun, Ph.D student
+ * 
+ *         Korea Advanced Institute of Science and Technology (KAIST)
+ * 
+ *         Real-time Embedded System Laboratory(RESL)
+ * 
+ *         bjw0829@kaist.ac.kr
+ */
 @Controller
 @RequestMapping("/querytest")
 public class QueryTest {
