b43fa4d831fc61f3a39a3624baed07a3e9d306cc
JaewookByun <bjw0829@gmail.com>
2014-10-14 00:08:20 +0900

    RESTful Put Method

--- epcis/src/main/java/org/oliot/epcis/service/rest/Resource.java
+++ epcis/src/main/java/org/oliot/epcis/service/rest/Resource.java
@@ -1,7 +1,23 @@
 package org.oliot.epcis.service.rest;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
 import org.oliot.epcis.configuration.ConfigurationServlet;
+import org.oliot.epcis.service.capture.CaptureService;
 import org.oliot.epcis.service.rest.mongodb.MongoRESTQueryService;
+import org.oliot.model.epcis.EPC;
+import org.oliot.model.epcis.SensingElementType;
+import org.oliot.model.epcis.SensingListType;
+import org.oliot.model.epcis.SensorEventType;
+import org.oliot.tdt.SimplePureIdentityFilter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,6 +45,78 @@ public class Resource {
 
 		}
 		return result;
+	}
+	
+	@RequestMapping(method = RequestMethod.PUT)
+	@ResponseBody
+	public String putEPCResource(@RequestParam(required = true) String target,
+			@RequestParam(required = true) String targetType,
+			@RequestParam(required = false) String eventTime,
+			@RequestParam(required = false) String finishTime,
+			@RequestParam(required = false) String sensorEPC,
+			@RequestParam(required = false) String sensorType,
+			@RequestParam(required = false) String sensorValue) {
+
+		
+		
+		if( !SimplePureIdentityFilter.isPureIdentity(target) )
+		{
+			return "targetObject or targetArea should follow Pure Identity Form";
+		}
+				
+		if( !targetType.equals("Object") && !targetType.equals("Area"))
+		{
+			return "targetType should be Object or Area";
+		}
+		
+		CaptureService cs = new CaptureService();
+		SensorEventType event = new SensorEventType();
+		
+		if( targetType.equals("Object"))
+		{
+			event.setTargetObject(target);
+		}
+		else if( targetType.equals("Area"))
+		{
+			event.setTargetArea(target);
+		}
+		
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat(
+					"yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+			if (eventTime != null)
+			{
+				GregorianCalendar eventCalendar = new GregorianCalendar();
+				eventCalendar.setTime(sdf.parse(eventTime));
+				XMLGregorianCalendar xmlEventTime = DatatypeFactory
+						.newInstance().newXMLGregorianCalendar(eventCalendar);
+				event.setEventTime(xmlEventTime);
+				event.setEventTimeZoneOffset(eventTime.substring(eventTime.length()-6));
+			}
+			if (finishTime != null)
+			{
+				GregorianCalendar finishCalendar = new GregorianCalendar();
+				finishCalendar.setTime(sdf.parse(finishTime));
+				XMLGregorianCalendar xmlFinishTime = DatatypeFactory
+						.newInstance().newXMLGregorianCalendar(finishCalendar);
+				event.setFinishTime(xmlFinishTime);
+			}	
+		} catch (ParseException e) {
+			return e.toString();
+		} catch (DatatypeConfigurationException e) {
+			return e.toString();
+		}
+		SensingElementType sensingElement = new SensingElementType();
+		sensingElement.setEpc(new EPC(sensorEPC));
+		sensingElement.setType(sensorType);
+		sensingElement.setValue(sensorValue);
+		List<SensingElementType> setList = new ArrayList<SensingElementType>();
+		setList.add(sensingElement);
+		SensingListType slt = new SensingListType();
+		slt.setSensingElement(setList);
+		event.setSensingList(slt);
+		cs.capture(event);
+		return "Event Captured";
 	}
 	
 	public String getEPCResourceOne(
--- epcis/target/epcis/index.jsp
+++ epcis/target/epcis/index.jsp
@@ -1,32 +1,28 @@
 <html>
 <body>
 	<h2>Welcome to Oliot-EPCIS</h2>
-	Spec:
+	Specification:
 	<br>-EPCIS v1.1
 	<br>-Java, Dynamic Web Service
 	<br>-Maven
 	<br>-Spring Framework
 	<br>
-	<br><h2>Service:</h2>
-	-Capture
-	<br>--Description: this service allows you to store your EPCIS
-	Document into your backend storage (e.g. Mongo DB)
-	<br>--method: post
-	<br>--contents: raw-xml-text, complying with
-	<a
-		href="http://www.gs1.org/gsmp/kc/epcglobal/epcis/epcis_1_1-schema-20140520/EPCglobal-epcis-1_1.xsd">EPCIS
-		v1.1 XML Schema</a>
-	<br>--url: http://{base-url}:{base-port}/epcis/Service/capture
-	<br>
-	<br>-ALE Capture
-	<br>--Description: this service allows you to store your ECReport
-	into your backend storage after converting it to EPCIS Document
-	<br>--method: post
-	<br>--contents: raw-xml-text, complying with
-	<a
-		href="http://www.gs1.org/gsmp/kc/epcglobal/ale/ale_1_1-schemas-20071202/EPCglobal-ale-1_1-ale.xsd">ALE
-		v1.1 XML Schema</a><br>--url: http://{base-url}:{base-port}/epcis/Service/aleCapture<br><br>
-
-
+	<br><h2>Service:</h2>	
+	<table border="1">
+		<tbody >
+			<tr> 
+				<td>SOAP Web Service</td>
+				<td><a href="./webservice">EPCIS v1.1 SOAP/HTTP Web Service</a></td>
+			</tr>
+			<tr>
+				<td>REST-Like Web Service</td>
+				<td><a href="./rest-like_service.html">EPCIS v1.1 compatible REST-Like Web Service</a></td>
+			</tr>
+			<tr>
+				<td>REST Web Service</td>
+				<td><a href="./rest_service.html">RESTful Web Service towards Resource Oriented Architecture</a></td>
+			</tr>
+		</tbody>
+	</table>
 </body>
 </html>
