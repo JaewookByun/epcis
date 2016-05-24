c57cd12b4589eb52532c61799f94f4a22597fac6
JaewookByun <bjw0829@gmail.com>
2016-05-24 12:10:19 +0900

    needless project removed

--- epcis-prototype-back/src/main/java/org/oliot/model/epcis/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// 이 파일은 JAXB(JavaTM Architecture for XML Binding) 참조 구현 2.2.8-b130911.1802 버전을 통해 생성되었습니다. 
-// <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a>를 참조하십시오. 
-// 이 파일을 수정하면 소스 스키마를 재컴파일할 때 수정 사항이 손실됩니다. 
-// 생성 날짜: 2016.04.28 시간 02:25:53 PM KST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:epcglobal:epcis:xsd:1")
-package org.oliot.model.epcis;
