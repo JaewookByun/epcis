64a9781ea7856cde10e4805776ef57398682be7a
JaewookByun <bjw0829@gmail.com>
2016-07-15 13:50:37 +0900

    VendorVersion: org.oliot.epcis-1.2.1

--- epcis/src/main/java/org/oliot/epcis/service/query/RESTLikeQueryService.java
+++ epcis/src/main/java/org/oliot/epcis/service/query/RESTLikeQueryService.java
@@ -300,7 +300,7 @@ public class RESTLikeQueryService implements ServletContextAware {
 		HttpHeaders responseHeaders = new HttpHeaders();
 		responseHeaders.add("Content-Type", "text/html; charset=utf-8");
 
-		return new ResponseEntity<>(new String(), responseHeaders, HttpStatus.OK);
+		return new ResponseEntity<>(new String("org.oliot.epcis-1.2.1"), responseHeaders, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/Poll/{queryName}", method = RequestMethod.GET)
