b9f46192ba64d2b4327797b3729a12ba365f51e0
JaewookByun <bjw0829@gmail.com>
2016-10-10 17:22:31 +0900

    EPCIS v1.2.3 released, ratified XSD applied, DocumentValidationService Enhanced, EPCIS Guideline Example Added

--- epcis/src/main/java/org/oliot/model/epcis/EPCISMasterDataExtensionType.java
+++ epcis/src/main/java/org/oliot/model/epcis/EPCISMasterDataExtensionType.java
@@ -8,33 +8,28 @@
 package org.oliot.model.epcis;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlAnyAttribute;
 import javax.xml.bind.annotation.XmlAnyElement;
 import javax.xml.bind.annotation.XmlType;
-import javax.xml.namespace.QName;
 import org.w3c.dom.Element;
 
 /**
  * <p>
- * Java class for VocabularyExtensionType complex type.
+ * Java class for EPCISMasterDataExtensionType complex type.
  * 
  * <p>
  * The following schema fragment specifies the expected content contained within
  * this class.
  * 
  * <pre>
- * &lt;complexType name="VocabularyExtensionType">
+ * &lt;complexType name="EPCISMasterDataExtensionType">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
  *         &lt;any processContents='lax' namespace='' maxOccurs="unbounded"/>
  *       &lt;/sequence>
- *       &lt;anyAttribute processContents='lax'/>
  *     &lt;/restriction>
  *   &lt;/complexContent>
  * &lt;/complexType>
@@ -43,13 +38,11 @@ import org.w3c.dom.Element;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "VocabularyExtensionType", propOrder = { "any" })
-public class VocabularyExtensionType {
+@XmlType(name = "EPCISMasterDataExtensionType", propOrder = { "any" })
+public class EPCISMasterDataExtensionType {
 
 	@XmlAnyElement(lax = true)
 	protected List<Object> any;
-	@XmlAnyAttribute
-	private Map<QName, String> otherAttributes = new HashMap<QName, String>();
 
 	/**
 	 * Gets the value of the any property.
@@ -79,24 +72,6 @@ public class VocabularyExtensionType {
 			any = new ArrayList<Object>();
 		}
 		return this.any;
-	}
-
-	/**
-	 * Gets a map that contains attributes that aren't bound to any typed
-	 * property on this class.
-	 * 
-	 * <p>
-	 * the map is keyed by the name of the attribute and the value is the string
-	 * value of the attribute.
-	 * 
-	 * the map returned by this method is live, and you can add new attribute by
-	 * updating the map directly. Because of this design, there's no setter.
-	 * 
-	 * 
-	 * @return always non-null
-	 */
-	public Map<QName, String> getOtherAttributes() {
-		return otherAttributes;
 	}
 
 }
