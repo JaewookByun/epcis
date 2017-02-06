26e3c776ca3bd28cc45194f758aa8af1cfc62b26
HaJaehee <lovesm135@naver.com>
2017-02-06 23:21:43 +0900

    added to capture event based on authorization feature and to request accessusers feature
    added to capture event based on authorization feature and to request
    accessusers feature

--- epcis/src/main/java/org/oliot/epcis/service/capture/SecureEventCapture.java
+++ epcis/src/main/java/org/oliot/epcis/service/capture/SecureEventCapture.java
@@ -1,13 +1,16 @@
 package org.oliot.epcis.service.capture;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Base64;
+import java.nio.charset.Charset;
 import java.util.Random;
 
 import javax.servlet.ServletContext;
@@ -26,6 +29,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.ServletContextAware;
+
+import com.mongodb.util.JSONParseException;
 
 /**
  * Copyright (C) 2014-2016 Jaewook Byun
@@ -56,7 +61,7 @@ public class SecureEventCapture implements ServletContextAware {
 	}
 
 	public ResponseEntity<?> asyncPost(String inputString) {
-		ResponseEntity<?> result = post(inputString, null, null, null);
+		ResponseEntity<?> result = post(inputString, null, null, null, null);
 		return result;
 	}
 
@@ -64,6 +69,7 @@ public class SecureEventCapture implements ServletContextAware {
 	@ResponseBody
 	public ResponseEntity<?> post(@RequestBody String inputString, 
 			@RequestParam(required = true) String userID,
+			@RequestParam(required = true) String epcisname,
 			@RequestParam(required = true) String accessToken,
 			@RequestParam(required = false) Integer gcpLength) {
 		JSONObject retMsg = new JSONObject();
@@ -85,49 +91,57 @@ public class SecureEventCapture implements ServletContextAware {
 		Random generator = new Random();
 		
 		//url of ac_api server
-		//String quri = "http://143.248.55.139:3001/user/"+userID+"/possess";
-		String quri = "http://127.0.0.1:3001/user/"+userID+"/possess";
+		String quri = "http://143.248.57.72:3001/user/"+userID+"/epcis/"+epcisname+"/furnish";
 		
 		//query to ac_api server
-		String qurlParameters = "epcisname=this_epcis"+(generator.nextInt(1000)+1)+"&epcisurl=127.0.0.1:"+(generator.nextInt(1000)+1);
+		String qurlParameters = "";		
 		String query_result = query_access_relation(quri, accessToken, qurlParameters);
 
 		//for debug, erase after implementing.
 		Configuration.logger.info(query_result);
+		query_result = query_result.replaceAll("[\"{} ]","").split(":")[1];
+		
+		boolean pass = (query_result.equals("yes"))?true:false;
 		
 		/* end of example for querying ac_api*/
 		
 //=============================================================================================		
 		
-		Configuration.logger.info(" EPCIS Document Capture Started.... ");
+		if (pass) {
+			Configuration.logger.info(" EPCIS Document Capture Started.... ");
 
-		// XSD based Validation
-		if (Configuration.isCaptureVerfificationOn == true) {
-			InputStream validateStream = CaptureUtil.getXMLDocumentInputStream(inputString);
-			boolean isValidated = CaptureUtil.validate(validateStream,
-					Configuration.wsdlPath + "/EPCglobal-epcis-1_2.xsd");
-			if (isValidated == false) {
-				// M63
-				return new ResponseEntity<>(new String("Error M63"), HttpStatus.BAD_REQUEST);
+			System.out.println(inputString);
+			// XSD based Validation
+			if (Configuration.isCaptureVerfificationOn == true) {
+				InputStream validateStream = CaptureUtil.getXMLDocumentInputStream(inputString);
+				boolean isValidated = CaptureUtil.validate(validateStream,
+						Configuration.wsdlPath + "/EPCglobal-epcis-1_2.xsd");
+				if (isValidated == false) {
+					// M63
+					return new ResponseEntity<>(new String("Error M63"), HttpStatus.BAD_REQUEST);
+				}
+				Configuration.logger.info(" EPCIS Document : Validated ");
+
 			}
-			Configuration.logger.info(" EPCIS Document : Validated ");
 
+			InputStream epcisStream = CaptureUtil.getXMLDocumentInputStream(inputString);
+			EPCISDocumentType epcisDocument = JAXB.unmarshal(epcisStream, EPCISDocumentType.class);
+
+			if (Configuration.isCaptureVerfificationOn == true) {
+				ResponseEntity<?> error = CaptureUtil.minorCheckDocumentHeader(epcisDocument);
+				if (error != null)
+					return error;
+			}
+
+			CaptureService cs = new CaptureService();
+			retMsg = cs.capture(epcisDocument, userID, "Friend", gcpLength);
+			Configuration.logger.info(" EPCIS Document : Captured ");
+		} else {
+			retMsg.put("Authorized", "no");
 		}
+		
 
-		InputStream epcisStream = CaptureUtil.getXMLDocumentInputStream(inputString);
-		EPCISDocumentType epcisDocument = JAXB.unmarshal(epcisStream, EPCISDocumentType.class);
-
-		if (Configuration.isCaptureVerfificationOn == true) {
-			ResponseEntity<?> error = CaptureUtil.minorCheckDocumentHeader(epcisDocument);
-			if (error != null)
-				return error;
-		}
-
-		CaptureService cs = new CaptureService();
-		retMsg = cs.capture(epcisDocument, userID, "Friend", gcpLength);
-		Configuration.logger.info(" EPCIS Document : Captured ");
-
-		if (retMsg.isNull("error") == true)
+		if (retMsg.isNull("error") == true && pass)
 			return new ResponseEntity<>(retMsg.toString(), HttpStatus.OK);
 		else
 			return new ResponseEntity<>(retMsg.toString(), HttpStatus.BAD_REQUEST);
@@ -142,12 +156,12 @@ public class SecureEventCapture implements ServletContextAware {
 		
 		try {
 		//String url = quri; //"http://143.248.55.139:3001/oauth/token";
-		String url = quri; //"http://127.0.0.1:3001/oauth/token";
+		String url = quri; //"http://143.248.57.72:3001/oauth/token";
 		URL obj = new URL(url);
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
 		//add reuqest header
-		con.setRequestMethod("POST");
+		con.setRequestMethod("GET");
 		con.setRequestProperty("Authorization", "Bearer "+qtoken);
 		con.setRequestProperty("Content-type", "application/x-www-form-urlencoded");
 		
@@ -155,15 +169,16 @@ public class SecureEventCapture implements ServletContextAware {
 		String urlParameters = qurlParameters;
 
 		// Send post request
-		con.setDoOutput(true);
-		DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-		wr.writeBytes(urlParameters);
-		wr.flush();
-		wr.close();
+		//con.setDoOutput(true);
+		//DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+		//wr.writeBytes(urlParameters);
+		
+		// Send get request		
+		System.out.println(con.getRequestMethod());
 
 		int responseCode = con.getResponseCode();
-		System.out.println("\nSending 'POST' request to URL : " + url);
-		System.out.println("Post parameters : " + urlParameters);
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		//System.out.println("Post parameters : " + urlParameters);
 		System.out.println("Response Code : " + responseCode);
 
 		BufferedReader in;
