f0e439aadc5e9d2fa451b2da0a3b5354f5183666
JaewookByun <bjw0829@gmail.com>
2019-03-07 16:46:04 +0900

    sync to async mongodb logic

--- epcis-x/src/main/java/org/oliot/epcis/service/EPCISServer.java
+++ epcis-x/src/main/java/org/oliot/epcis/service/EPCISServer.java
@@ -1,11 +1,6 @@
 package org.oliot.epcis.service;
 
-import org.bson.BsonDocument;
 import org.oliot.epcis.service.capture.CaptureService;
-
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
@@ -13,14 +8,14 @@ import io.vertx.core.Future;
 import io.vertx.core.Vertx;
 import io.vertx.core.http.HttpServer;
 import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
-public class EPCISServer extends AbstractVerticle{
+public class EPCISServer extends AbstractVerticle {
 
 	public static MongoClient mClient;
-	public static MongoDatabase mDatabase;
-	public static MongoCollection<BsonDocument> eventCollection;
 
 	@Override
 	public void start(Future<Void> future) throws Exception {
@@ -42,14 +37,12 @@ public class EPCISServer extends AbstractVerticle{
 
 		server.requestHandler(router).listen(8080);
 	}
-	
+
 	public static void main(String[] args) {
-		// Initialize Backend
-		mClient = new MongoClient();
-		mDatabase = mClient.getDatabase("epcis");
-		eventCollection = mDatabase.getCollection("EventData", BsonDocument.class);
 
 		Vertx vertx = Vertx.vertx();
+		mClient = MongoClient.createShared(vertx, new JsonObject().put("db_name", "epcis"));
+
 		DeploymentOptions options = new DeploymentOptions().setInstances(16);
 		vertx.deployVerticle("org.oliot.epcis.service.EPCISServer", options);
 	}
